f0990d8cd8dcf3ac0a1a0c60b386b7020ac6f91f
ticktoo <office@ticktoo.com>
2013-09-29 23:50:18 +0200

    new function: search()

--- libmyabotv.php
+++ libmyabotv.php
@@ -47,6 +47,123 @@
 			return($c);
 		}
 		
+		function enumerateStations()	{
+			$s = array(
+				"233" => "2PLUS2",
+				"242" => "3PLUS",
+				"1" => "3SAT",
+				"58" => "ANIXE",
+				"2" => "ARD",
+				"4" => "ARD EINSFESTIVAL",
+				"5" => "ARD EINSPLUS",
+				"6" => "ARTE",
+				"53" => "ARTEFR",
+				"215" => "ATV",
+				"7" => "BAY3",
+				"9" => "BBC WORLD",
+				"48" => "BIBELTV",
+				"8" => "BR ALPHA",
+				"10" => "CNBC",
+				"11" => "CNN",
+				"47" => "COMEDYCENTRAL",
+				"45" => "DAS4",
+				"49" => "DELUXEMUSIC",
+				"38" => "DMAX",
+				"60" => "ERF",
+				"12" => "EURONEWS",
+				"13" => "EUROSPORT",
+				"14" => "GOTV",
+				"15" => "HR3",
+				"50" => "IMUSIC",
+				"229" => "ITALIA1",
+				"16" => "KABEL 1",
+				"17" => "KIKA",
+				"56" => "KTV",
+				"216" => "M6",
+				"18" => "MDR",
+				"21" => "N24",
+				"22" => "NDR",
+				"44" => "NICKELODEON",
+				"20" => "NTV",
+				"193" => "ORF1",
+				"194" => "ORF2",
+				"218" => "ORF3",
+				"219" => "ORFSPORTPLUS",
+				"195" => "PERWY",
+				"24" => "PHOENIX",
+				"25" => "PRO7",
+				"198" => "PULS4",
+				"227" => "RAI1",
+				"228" => "RAI2",
+				"231" => "RAI3",
+				"26" => "RBB",
+				"250" => "RIC",
+				"27" => "RTL",
+				"28" => "RTL2",
+				"230" => "RTLNITRO",
+				"196" => "RTRPLANETA",
+				"29" => "SAT1",
+				"252" => "SAT1GOLD",
+				"243" => "SF1",
+				"244" => "SF2",
+				"172" => "SIXX",
+				"128" => "SPORT1",
+				"30" => "SRTL",
+				"63" => "STV",
+				"31" => "SW3",
+				"245" => "TAGESSCHAU24",
+				"32" => "TELE5",
+				"213" => "TF1",
+				"246" => "TRT",
+				"34" => "TV5",
+				"55" => "TVPHISTORIA",
+				"61" => "TVPINFO",
+				"54" => "TVPKULTURA",
+				"51" => "TVPOLONIA",
+				"138" => "UKBBC",
+				"173" => "UKBBC2",
+				"155" => "UKBBC3",
+				"174" => "UKBBC4",
+				"214" => "UKCBBC",
+				"253" => "UKCBEEBIES",
+				"165" => "UKCBSACTION",
+				"197" => "UKCBSDRAMA",
+				"169" => "UKCHANNEL4",
+				"163" => "UKE4",
+				"162" => "UKFILM4",
+				"164" => "UKFIVE",
+				"199" => "UKHORROR",
+				"157" => "UKITV",
+				"158" => "UKITV2",
+				"159" => "UKITV3",
+				"175" => "UKITV4",
+				"212" => "UKMORE4",
+				"232" => "USBOUNCETV",
+				"251" => "USIONTV",
+				"254" => "USNJTV",
+				"255" => "USWABC",
+				"66" => "USWCBS",
+				"75" => "USWFUT",
+				"68" => "USWNBC",
+				"71" => "USWNJU",
+				"74" => "USWNYE",
+				"67" => "USWNYW",
+				"249" => "USWPIX",
+				"69" => "USWWOR",
+				"76" => "USWXTV",
+				"35" => "VIVA",
+				"36" => "VOX",
+				"217" => "W9",
+				"37" => "WDR",
+				"39" => "ZDF",
+				"41" => "ZDF INFO",
+				"180" => "ZDFKULTUR",
+				"64" => "ZDF NEO",			
+			);
+			
+			return($s);
+		}
+		
 		function enumerateShowsByGenre($genre, $days = 7)	{
 			$request = "GET /topshows-genre/$genre/$days/ HTTP/1.0\n";
 			$request.= "Host: myabo.tv\n";
@@ -67,6 +184,61 @@
 				$ret[] = new MyAboTvShow($this->authToken, $url);
 			}
 			return($ret);
+		}
+		
+		function search($term, $station = '', $cut = false)	{
+			$term = urlencode($term);
+			
+			if((int)$station == 0)	{
+				$station = $this->resolveStationName($station);
+			}
+			if($cut)	{
+				$c = "&cut=on";
+			}
+			
+			$ret = false;
+			$page = 1;
+			
+			while(1 == 1)	{
+				$url 	 = "http://myabo.tv/search/?page=$page&order=startdate&order_type=ASC&term=$term&download=on&date=&genre=&station=$station&search_languages=1&quality=0".$c."&timerange=";
+				echo("# $url\n");
+				$request = "GET $url HTTP/1.0\n";
+				$request.= "Host: myabo.tv\n";
+				$request.= "Cookie: sessionid=".$this->authToken."\n\n";
+		
+				$response = "";
+				$sh = fsockopen("myabo.tv", "80");	
+				fputs($sh, $request);
+				while($buffer = fgets($sh, 1024))	{
+					$response.= $buffer;
+				}
+				fclose($sh);
+			
+				$tmp = array();
+				$pattern = "|<a  class=\"dl\" href=\"(.*?)\" style=\"display:inline;\">Download</a>|i";
+				preg_match_all($pattern, $response, $tmp);
+				
+				if(count($tmp[1]) > 0)	{
+					foreach((array)$tmp[1] as $index=>$url)	{
+						$ret[] = new MyAboTvShow($this->authToken, $url);
+					}
+					$page++;
+				} else {
+					break;
+				}
+			}
+			return($ret);			
+		}
+		
+		private function resolveStationName($stationname)	{
+			$stationname = strtolower($stationname);
+			$stations	 = $this->enumerateStations();
+			foreach($stations as $id=>$station)	{
+				if($stationname == strtolower($station))	{
+					return($id);
+				} 
+			}
+			return(false);
 		}
 	}
 	
@@ -140,7 +312,26 @@
 				foreach($hashtable as $index=>$key)	{
 					$out[] = $this->files[$index];
 				}	
-				$this->files = $out;			
+				$this->files = $out;						
+				
+			// Date
+				if(isset($this->files[0]) && isset($this->files[0]->url))	{
+					$fn = $this->files[0]->url;
+					$pattern = "|.*_([0-9][0-9]\.[0-9][0-9]\.[0-9][0-9]_[0-9][0-9]-[0-9][0-9])_.*|i";
+					preg_match($pattern, $fn, $tmp5);
+						$tmp6 = explode("_", $tmp5[1]);
+						$d = explode(".", $tmp6[0]);
+						$t = explode("-", $tmp6[1]);
+					
+						$ts = mktime($t[0], $t[1], 0, $d[1], $d[2], (int)$d[0] +2000);
+						$this->timestamp = $ts;
+						$this->datetime = date("d.m.Y H:i", $ts);
+				} else {
+					print_r($this);
+					die("--\n");
+				}
+				
+	
 		}
 	}
 	
